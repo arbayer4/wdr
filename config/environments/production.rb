cecc3812889eaf4ca06c720c75d20be86f3a7e3c
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-12 10:57:04 -0400

    grasping

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,13 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # This also configures session_options for use below
-config.session_store :cookie_store, key: '_interslice_session'
+#   # This also configures session_options for use below
+# config.session_store :cookie_store, key: '_interslice_session'
 
-# Required for all session management (regardless of session_store)
-config.middleware.use ActionDispatch::Cookies
+# # Required for all session management (regardless of session_store)
+# config.middleware.use ActionDispatch::Cookies
 
-config.middleware.use config.session_store, config.session_options
+# config.middleware.use config.session_store, config.session_options
 
 
 config.action_mailer.delivery_method = :smtp
