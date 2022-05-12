e948d4917a33e228b77797b56fe1c9764d11a81c
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-12 08:12:36 -0400

    added more domains

--- config.ru
+++ config.ru
@@ -10,12 +10,7 @@ use Rack::Cors do
 
 
   allow do
-    origins "http://localhost:3001"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins "bloomingtonwdr.com"
+    origins "https://bloomingtonwdr.com", "bloomingtonwdr.com", "www.bloomingtonwdr.com", "https://main--adorable-heliotrope-49d3d0.netlify.app/", "https://www.bloomingtonwdr.com", "http://localhost:3001"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "http://localhost:3001"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
 
   allow do
-    origins "bloomingtonwdr.com"
+    origins "https://bloomingtonwdr.com", "bloomingtonwdr.com", "www.bloomingtonwdr.com", "https://main--adorable-heliotrope-49d3d0.netlify.app/", "https://www.bloomingtonwdr.com", "http://localhost:3001"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
