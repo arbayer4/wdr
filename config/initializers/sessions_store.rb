5f754c55952a1cc292683d139ff39d510ffa64fd
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-12 11:48:52 -0400

    tld

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_wdr_app", domain: :all, secure: true
+  Rails.application.config.session_store :cookie_store, key: "_wdr_app", domain: :all, :tld_length => 2
 else
   Rails.application.config.session_store :cookie_store, key: "_wdr_app"
 end
