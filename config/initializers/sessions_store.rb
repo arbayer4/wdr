022e7e31644878c96eeb109f2ae721d25f4f597a
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-12 12:11:58 -0400

    yada yada

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_wdr_app", domain: :all, :tld_length => 2
+  Rails.application.config.session_store :cookie_store, key: "_wdr_app", domain: :all, :tld_length => 2, same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_wdr_app"
 end
