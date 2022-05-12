e411e8ceaf1d70f43c41a72e8eb9196a5f4d7ec7
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-12 10:51:51 -0400

    more stuff

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -8,6 +8,7 @@ module CurrentUserConcern
   def set_current_user
     logger.info "Setting current User"
     logger.info "Session: #{session.inspect}"
+    logger.info "Session user concern: #{session[:user_id]}"
     if session[:user_id]
       logger.info "session user"
       @current_user = User.find(session[:user_id])
