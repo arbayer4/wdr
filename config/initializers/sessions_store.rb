e81693c51249ba7be023054cdf6c6665f1ec093f
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-12 11:14:36 -0400

    update session

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_wdr_app", domain: "wdr-bloomington.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_wdr_app", domain: "wdr-bloomington.herokuapp.com", secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_wdr_app"
 end
